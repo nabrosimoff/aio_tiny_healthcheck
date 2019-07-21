6942db13786b1160fd1676892c3a0d1b8b3c8fa1
Nikolay Abrosimov <nabrosimov@nwave.io>
2019-07-21 19:04:22 +0300

    setup.py is updated

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="aio_tiny_healthcheck",
-    packages=find_packages()
+    version="0.1.0",
+    author="Nikolai Abrosimov",
+    author_email="nikolay.abrosimoff@gmail.com",
+    description="Tiny asynchronous implementation of healthcheck provider and http-server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nabrosimoff/aio_tiny_healthcheck",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
 )
