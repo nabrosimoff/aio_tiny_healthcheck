00d740b20c598072c56718becda36042a965c12d
Nikolay Abrosimov <nabrosimov@nwave.io>
2019-07-22 11:09:07 +0300

    Minor changes in codecov and packaging settings

--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ setuptools.setup(
     url="https://github.com/nabrosimoff/aio_tiny_healthcheck",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
